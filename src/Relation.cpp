e079b5901060711403eb10d22323b6a46e33b387
Spencer Tuft <spencer.tuft@gmail.com>
2018-12-12 15:58:39 -0700

    Fix bug

--- src/Relation.cpp
+++ src/Relation.cpp
@@ -141,7 +141,7 @@ Relation Relation::join(std::string name, Relation relation) {
   auto join_on = [](List h1, List h2) {
     std::vector<std::pair<int, int>> join_columns;
     for (size_t i = 0; i < h1.size(); i++) {
-      for (size_t j = 0; j < h1.size(); j++) {
+      for (size_t j = 0; j < h2.size(); j++) {
         if (h1[i] == h2[j]) {
           std::pair<int, int> temp_pair;
           temp_pair.first = static_cast<int>(i);
